e2cf4038c712d2d8bf2192fac002baf58965f977
ThePrimeagen <the.primeagen@aol.com>
2025-03-27 22:26:37 +0200

    3: count words in the document

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 def get_book_text(file_p):
     with open(file_p) as f:
         file_contents = f.read()
-    print(file_contents)
+    #print(file_contents)
     return file_contents
+
+def word_count(words):
+    #count the number of words in the book
+    #print(words)
+    split_words = words.split()
+    word_count = len(split_words)
+    return word_count
+
 
 def main():
     file_path = "books/frankenstein.txt"
-    get_book_text(file_path)
+    book = get_book_text(file_path)
+    total = word_count(book)
+    print(f"{total} words found in the document")
     
 main()
     
